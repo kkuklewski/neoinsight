530a7070e320d0ef2b2afafc89dd3225dcd4da89
kamilfikou <kkuklewski@gmail.com>
2023-08-14 13:10:32 +0200

    dark theme

--- src/app/components/HeadingComponent.jsx
+++ src/app/components/HeadingComponent.jsx
@@ -2,7 +2,7 @@ export default function HeadingComponent({ text }) {
     return (
         <>
             <h2 className="text-base font-semibold leading-7 text-black-pearl-400">Section description</h2>
-            <h2 className='text-4xl mb-6 tracking-tight text-white sm:text-6xl'>
+            <h2 className='text-4xl mb-6 tracking-tight sm:text-6xl'>
                 {text}
             </h2>
         </>
--- src/app/sections/sponsors.jsx
+++ src/app/sections/sponsors.jsx
@@ -22,12 +22,12 @@ export default function Sponsors() {
                 <div className="mx-auto px-6 lg:px-8">
                     <div className="mx-auto max-w-2xl lg:mx-0">
                         <HeadingComponent text="For sponsors/CRO" />
-                        <p className="mt-6 text-lg leading-8 text-gray-300">
+                        <p className="mt-6 text-lg leading-8">
                             We offer cooperation in the recruitment of patients for your clinical trials in hematology and oncology indications.
                         </p>
                     </div>
                     <div className="mx-auto mt-10 max-w-2xl lg:mx-0 lg:max-w-none">
-                        <div className="grid grid-cols-1 gap-x-8 gap-y-6 text-base font-semibold leading-7 text-white sm:grid-cols-2 md:flex lg:gap-x-10">
+                        <div className="grid grid-cols-1 gap-x-8 gap-y-6 text-base font-semibold leading-7 sm:grid-cols-2 md:flex lg:gap-x-10">
                             {links.map((link) => (
                                 <a key={link.name} href={link.href}>
                                     {link.name} <span aria-hidden="true">&rarr;</span>
@@ -37,8 +37,8 @@ export default function Sponsors() {
                         <dl className="mt-16 grid grid-cols-1 gap-8 sm:mt-20 sm:grid-cols-2 lg:grid-cols-4">
                             {stats.map((stat) => (
                                 <div key={stat.name} className="flex flex-col-reverse">
-                                    <dt className="text-base leading-7 text-gray-300">{stat.name}</dt>
-                                    <dd className="text-2xl font-bold leading-9 tracking-tight text-white">{stat.value}</dd>
+                                    <dt className="text-base leading-7">{stat.name}</dt>
+                                    <dd className="text-2xl font-bold leading-9 tracking-tight">{stat.value}</dd>
                                 </div>
                             ))}
                         </dl>
